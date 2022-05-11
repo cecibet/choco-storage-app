466a32c6e770351e6380d45a00c39acfbad71223
cecibet <ceci.bet21@gmail.com>
2022-05-11 17:42:59 -0300

    Create App

---
+++ src/components/Login.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+import "./Login.css";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+
+  const [password, setPassword] = useState("");
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const validateForm = () => {
+    return email.length > 0 && password.length > 0;
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="Login">
+      <form onSubmit={handleSubmit}>
+        <label>Email  </label>
+
+        <input
+          autoFocus
+          type="email"
+          value={email}
+          onChange={handleEmail}
+        ></input>
+        <br></br>
+        <label>Contraseña  </label>
+        <input
+          type="password"
+          value={password}
+          onChange={handlePassword}
+        ></input>
+        <br></br>
+
+        <button type="submit" disabled={!validateForm()}>
+          Ingresar
+        </button>
+      </form>
+    </div>
+  );
+}
