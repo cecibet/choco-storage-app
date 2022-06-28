d52d7b5479bec1ab044382a47f739e36c59b878e
fabriziodelisa <fabrizio.de.lisa@gmail.com>
2022-06-28 00:07:53 -0300

    dropdown

--- src/Components/SharedComponents/Dropdown/Dropdown.js
+++ src/Components/SharedComponents/Dropdown/Dropdown.js
@@ -1,21 +1,12 @@
 import React from "react";
-<<<<<<< HEAD
-import "./Dropdown.css";
-=======
 import styles from "./dropdown.module.css";
->>>>>>> master
 
 
 
 const Dropdown = ({ options, onChange, label }) => {
   return (
-<<<<<<< HEAD
-    <div className="custom-select">
-      <label>
-=======
     <div className={styles.customSelect}>
       <label className={styles.label}>
->>>>>>> master
         {label}
         <select onChange={onChange}>
           <option disabled selected>
